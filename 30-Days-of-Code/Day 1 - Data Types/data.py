43357cdfe31ae2f5a98a7cc51f7db88c5b2efcc5
Harry Chong <hchong.16@gmail.com>
2020-05-18 01:04:47 -0400

    Initialize Repo

---
+++ 30-Days-of-Code/Day 1 - Data Types/data.py
@@ -0,0 +1,26 @@
+ #!/bin/python3
+# Author: Harry Chong
+# Data: 5/17/2020
+# Day 1: Data Types
+# https://www.hackerrank.com/challenges/30-data-types/problem
+
+# Declare second integer, double, and String variables.
+i = 4
+d = 4.0
+s = "HackerRank "
+
+# Read and save an integer, double, and String to your variables.
+# Note: This block of code is more detailed. It will not work through the submission
+input_1 = int(input())
+input_2 = float(input())
+input_3 = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(input_1 + i)
+
+# Print the sum of the double variables on a new line.
+print(input_2 + d)
+
+# Concatenate and print the String variables on a new line. The 's' variable 
+# above should be printed first.
+print(s + input_3)
---
+++ README.md
@@ -0,0 +1,4 @@
+# HackerRank
+Repo to hold all my code solution for HackerRank.
+
+Profile: https://www.hackerrank.com/hchong
